b877472f5cd60411ca065a2837b55aae53fd3bad
Sezen <aysezen6@gmail.com>
2023-06-19 20:54:01 +0200

    V2

--- src/test/java/test/C11_Get_ExpectedDataOlusturma.java
+++ src/test/java/test/C11_Get_ExpectedDataOlusturma.java
@@ -1,7 +1,13 @@
 package test;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C11_Get_ExpectedDataOlusturma {
 
@@ -35,9 +41,22 @@ public class C11_Get_ExpectedDataOlusturma {
         expData.put("title","dolor sint quo a velit explicabo quia nam");
         expData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
 
+        System.out.println("expData = " + expData);
 
+        // 3 - Response'i kaydet
 
+        Response response = given().when().get(url);
 
+        response.prettyPeek(); //prettyPrintten farkli olarak size response ile ilgili tum degerleri dondurur
+
+        // 4 - Assertion
+
+        JsonPath respJP = response.jsonPath();
+
+        assertEquals(expData.get("userId"), respJP.get("userId"));
+        assertEquals(expData.get("id"), respJP.get("id"));
+        assertEquals(expData.get("title"), respJP.get("title"));
+        assertEquals(expData.get("body"), respJP.get("body"));
 
     }
 
---
+++ src/test/java/test/C12_Post_ExpectedDataVeJsonPathIleAssertion.java
@@ -0,0 +1,84 @@
+package test;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C12_Post_ExpectedDataVeJsonPathIleAssertion {
+
+    /*
+    https://restful-booker.herokuapp.com/booking url’ine
+    asagidaki body'ye sahip bir POST request gonderdigimizde
+    donen response’un id disinda asagidaki gibi oldugunu test edin.
+    	                Request body
+    	           {
+    	                "firstname" : "Ahmet",
+    	                "lastname" : “Bulut",
+    	                "totalprice" : 500,
+    	                "depositpaid" : false,
+    	                "bookingdates" : {
+    	                         "checkin" : "2021-06-01",
+    	                         "checkout" : "2021-06-10"
+    	                                  },
+    	                "additionalneeds" : "wi-fi"
+    	            }
+
+
+    	            	Response Body - Expected Body
+    	           {
+                    "bookingid":24,
+                    "booking":{
+                        "firstname":"Ahmet",
+                        "lastname":"Bulut",
+                        "totalprice":500,
+                        "depositpaid":false,
+                        "bookingdates":{
+                            "checkin":"2021-06-01",
+                            "checkout":"2021-06-10"
+                                        }
+                        ,
+                        "additionalneeds":"wi-fi"
+                             }
+                    }
+*/
+
+    @Test
+    public void post01(){
+
+        // 1 - Url ve Request Body hazirla
+
+        String url = "https://restful-booker.herokuapp.com/booking";
+
+        /*
+                   {
+    	                "firstname" : "Ahmet",
+    	                "lastname" : “Bulut",
+    	                "totalprice" : 500,
+    	                "depositpaid" : false,
+    	                "bookingdates" : {
+    	                         "checkin" : "2021-06-01",
+    	                         "checkout" : "2021-06-10"
+    	                                  },
+    	                "additionalneeds" : "wi-fi"
+    	            }
+         */
+
+        JSONObject bookingdates = new JSONObject();
+
+        bookingdates.put("checkin" , "2021-06-01");
+        bookingdates.put("checkout" , "2021-06-10");
+
+        JSONObject reqBody = new JSONObject();
+
+        reqBody.put("firstname" , "Ahmet");
+        reqBody.put("lastname" , "Bulut");
+        reqBody.put("totalprice" , 500);
+        reqBody.put("depositpaid" , false);
+        reqBody.put("additionalneeds" , "wi-fi");
+        reqBody.put("bookingdates" , bookingdates);
+
+    }
+
+
+
+
+}
