2cdbd98d5279680d1290a66740ff833bb1904e1c
Sezen <aysezen6@gmail.com>
2023-06-20 21:19:40 +0200

    V2

--- src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
+++ src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
@@ -2,10 +2,12 @@ package test;
 
 import baseUrl.JsonPlaceHolderBaseURL;
 import io.restassured.response.Response;
-import org.codehaus.groovy.control.io.ReaderSource;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseURL {
     //Class icinde 3 Test metodu olusturun ve asagidaki testleri yapin
@@ -43,15 +45,58 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseURL {
         // 3 - Response'i kaydet
 
         Response response = given().spec(specJsonPlace).when().get("/{pp1}");
-        response.prettyPrint();
+        // response.prettyPrint();
+
+        // 4 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("title", hasSize(100));
 
     }
     @Test
     public void get02(){
+        /*
+        2- https://jsonplaceholder.typicode.com/posts/44 endpointine bir GET
+            request gonderdigimizde donen response’un status code’unun 200 oldugunu
+            ve “title” degerinin “optio dolor molestias sit” oldugunu test edin
+         */
+
+        // 1 - Url hazirla
+
+        specJsonPlace.pathParams("pp1","posts","pp2",44);
+
+        // 2 - Expected Data hazirla
+
+        // 3 - Response'i kaydet
+
+        Response response = given().spec(specJsonPlace).when().get("/{pp1}/{pp2}");
+
+        response.prettyPrint();
+
+        // 4 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("title", equalTo("optio dolor molestias sit") );
+
 
     }
     @Test
     public void delete01(){
+         /*
+        3- https://jsonplaceholder.typicode.com/posts/50 endpointine bir DELETE
+            request gonderdigimizde donen response’un status code’unun 200 oldugunu ve
+            response body’sinin null oldugunu test edin
+         */
+
+        // 1 - Url hazirla
+
+
 
     }
 }
